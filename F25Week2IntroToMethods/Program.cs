db5afb87b8977abba0e7fba092cbd00cbac36706
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-10 09:32:22 -0400

    array

--- F25Week2IntroToMethods/Program.cs
+++ F25Week2IntroToMethods/Program.cs
@@ -6,22 +6,26 @@
         {
             int n = 5;
             string s = "hello";
+            int[] a = { 1, 2, 3 };
 
             Console.WriteLine("Before changes: \n");
             Console.WriteLine("n = " + n);
             Console.WriteLine("s = " + s);
+            Console.WriteLine("a[0] = " + a[0]);
 
-            ChangeValue(n, s);
+            ChangeValue(n, s, a);
 
             Console.WriteLine("\n\nAfter changes: \n");
             Console.WriteLine("n = " + n);
             Console.WriteLine("s = " + s);
+            Console.WriteLine("a[0] = " + a[0]);
         }
 
-        static void ChangeValue(int num, string str)
+        static void ChangeValue(int num, string str, int[] arr)
         {
             num = 100;
             str = "bye";
+            arr[0] = 100;
         }
     }
 }
