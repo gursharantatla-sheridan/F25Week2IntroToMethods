61d1385039e0c97088b7ddd63f4b1ab2a1025be5
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-10 09:48:53 -0400

    output param

--- F25Week2IntroToMethods/Program.cs
+++ F25Week2IntroToMethods/Program.cs
@@ -2,6 +2,8 @@
 {
     internal class Program
     {
+        //int x;
+
         static void Main(string[] args)
         {
             int n = 5;
@@ -13,15 +15,34 @@
             Console.WriteLine("s = " + s);
             Console.WriteLine("a[0] = " + a[0]);
 
-            ChangeValue(n, s, a);
+            ChangeValue(ref n, s, a);
 
             Console.WriteLine("\n\nAfter changes: \n");
             Console.WriteLine("n = " + n);
             Console.WriteLine("s = " + s);
             Console.WriteLine("a[0] = " + a[0]);
+
+            Console.WriteLine("\n\n\n");
+
+
+            int rad = 5;
+            double circum, area;
+
+            Calculate(rad, out circum, out area);
+
+            Console.WriteLine("Circumference = " + circum);
+            Console.WriteLine("Area = " + area);
         }
 
-        static void ChangeValue(int num, string str, int[] arr)
+        static void Calculate(int r, out double cir, out double ar)
+        {
+            cir = 2 * Math.PI * r;
+            ar = Math.PI * r * r;
+        }
+
+
+
+        static void ChangeValue(ref int num, string str, int[] arr)
         {
             num = 100;
             str = "bye";
