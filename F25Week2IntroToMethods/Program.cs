068497106fc621503031f99587bfa6196aa622f2
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-10 09:20:12 -0400

    int, str

--- F25Week2IntroToMethods/Program.cs
+++ F25Week2IntroToMethods/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = 5;
+            string s = "hello";
+
+            Console.WriteLine("Before changes: \n");
+            Console.WriteLine("n = " + n);
+            Console.WriteLine("s = " + s);
+
+            ChangeValue(n, s);
+
+            Console.WriteLine("\n\nAfter changes: \n");
+            Console.WriteLine("n = " + n);
+            Console.WriteLine("s = " + s);
+        }
+
+        static void ChangeValue(int num, string str)
+        {
+            num = 100;
+            str = "bye";
         }
     }
 }
